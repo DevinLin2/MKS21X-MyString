4fb28684866aaa1b3b8b8fd7e555ea63ba177631
Devin Lin <Dlin01@stuy.edu>
2018-11-25 18:13:37 -0500

    added a compareTo

--- MyString.java
+++ MyString.java
@@ -6,13 +6,13 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
       data[i] = s.chatAt(i);
     }
   }
-  public char charAt(int index){ //implement indexOutOfBounds
+  public char charAt(int index) throws IndexOutOfBoundsException{ //implement indexOutOfBounds
     return data[index];
   }
   public int length(){
     return data.length;
   }
-  public String subSequence(int start, int end){
+  public String subSequence(int start, int end) throws IndexOutOfBoundsException{
     String subSequence = "";
     for (int i = start; i < end; i++){
       ans += data.charAt[i];
@@ -25,5 +25,24 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
       ans += data[i];
     }
     return ans;
+  }
+  public int compareTo(CharSequence value) throws NullPointerException{
+    if (data.length > value.length()){
+      return 1;
+    }
+    if (data.length < value.length()){
+      return -1;
+    }
+    for (int i = 0; i < data.length; i ++){
+      if (!(data[i] == value.charAt(i))){
+        if (data[i] > value.charAt(i)){
+          return 1;
+        }
+        if (data[i] < value.charAt(i)){
+          return -1;
+        }
+      }
+    }
+    return 0;
   }
 }
